4615061c48883db2b5d80fbf5d8e761cda7acc21
Ylanna <a114280@alunos.uminho.pt>
2025-11-06 16:00:50 +0000

    avancaObjeto feito, Objeto Disparo dando erro

--- lib/Tarefa3.hs
+++ lib/Tarefa3.hs
@@ -60,7 +60,49 @@ avancaMinhoca e i minhoca = case encontraPosicaoMatriz posicaoFinal mapa of
 
 -- | Para um dado estado, dado o índice de um objeto na lista de objetos e o estado desse objeto, retorna o novo estado do objeto no próximo tick ou, caso o objeto expluda, uma lista de posições afetadas com o dano associado.
 avancaObjeto :: Estado -> NumObjeto -> Objeto -> Either Objeto Danos
-avancaObjeto e i o = 
+avancaObjeto e i o = case o of
+    Barril pos explode
+        | explode -> Right [(pos, 50)] -- Barril explode
+        | if (estaNoSolo pos mapa) then Left o -- Barril ta no chao  
+                else
+            let newPos = movePosicao Sul pos
+            in case encontraPosicaoMatriz newPos mapa of
+                Just Agua -> Right [(newPos, 0)] -- Barril some na agua
+                Nothing -> Right [(pos, 0)] -- Barril caiu do mapa
+                otherwise -> Left (Barril newPos False) -- Barril ta no ar e cai
+    
+    Disparo pos dir tempo dono -> case tipoDisparo of
+        Jetpack -> Right [(pos, 0)] -- Some jetpack e usa ele
+
+        Escavadora
+            Just Terra -> Right [(pos, 25)] -- Destroi terra
+            _ -> Right [(pos, 0)] -- em qlqr outro terreno some
+        Bazuca -> case tempo of
+            Just t | t <= 0 -> Right [(pos, 75)]  -- Bazuca explode com tempo 0
+            Just t -> Left (Disparo pos dir tipo (Just (t-1)) dono)  -- cronometro
+            Nothing -> 
+                let newPos = movePosicao dir pos
+                in case encontraPosicaoMatriz newPos mapa of
+                    Just Pedra -> Right [(pos, 75)]  -- Explode na pedra
+                    Just Terra -> Right [(pos, 75)]  -- Explode na terra
+                    Just Ar -> Left (Disparo newPos dir tipo Nothing dono)  -- Continua movimento
+                    _ -> Right [(pos, 75)]  -- Explode em outros casos
+        
+        Mina -> case encontraPosicaoMatriz pos mapa of
+            Just Terra -> Right [(pos, 40)]  -- Mina explode na terra
+            Just Ar | estaNoSolo pos mapa -> Left o  -- Mina fica no chão
+            Just Ar -> Left (Disparo (movePosicao Sul pos) dir tipo tempo dono)  -- Mina cai
+            _ -> Right [(pos, 40)]  -- Mina explode em outros casos
+        
+        Dinamite -> case tempo of
+            Just t | t <= 0 -> Right [(pos, 100)]  -- Dinamite explode com tempo 0
+            Just t -> Left (Disparo pos dir tipo (Just (t-1)) dono)  -- cronometro
+            Nothing -> Right [(pos, 100)] 
+    where
+        mapa = mapaEstado e
+
+
+        
 
 -- | Para uma lista de posições afetadas por uma explosão, recebe um estado e calcula o novo estado em que esses danos são aplicados.
 aplicaDanos :: Danos -> Estado -> Estado
