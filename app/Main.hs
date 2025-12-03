6cf6d6cfe0454f1dcec4d5725823f08c6c57df96
PedroCoutinho <edr0c0de@protonmail.com>
2025-12-03 21:17:32 +0000

    fixes

--- app/Main.hs
+++ app/Main.hs
@@ -36,22 +36,7 @@ main = do
   barril <- loadBMP "app/sprites/Barril.bmp"
   bazuca <- loadBMP "app/sprites/Bazuca.bmp"
   let tiles = [grass, water, stone, worm, morto, barril, bazuca,sky]
-=======
---Tiles
-  grass <- loadBMP "app/sprites/Grass_1_16x16.bmp" -- 0
-  water <- loadBMP "app/sprites/Water_1_16x16.bmp" -- 1
-  stone <- loadBMP "app/sprites/Stone_1_16x16.bmp" -- 2
-  sky <- loadBMP "app/sprites/Ice_18_16x16.bmp" -- 8
---Minhocas e objetos
-  worm <- loadBMP "app/sprites/Worm.bmp" -- 3
-  morto <- loadBMP "app/sprites/Dead.bmp" -- 4
-  barril <- loadBMP "app/sprites/Barril.bmp" -- 5
-  bazuca <- loadBMP "app/sprites/Bazuca.bmp" -- 6
-  dinamite <- loadBMP "app/sprites/Dinamite.bmp" -- 7
-  mina <- loadBMP "app/sprites/Mina.bmp" -- 9
-  
-  let tiles = [grass, water, stone, worm, morto, barril, bazuca, dinamite, sky, mina]
->>>>>>> c46179ed865b6dafa9e3f73fd68655d00168a28b
+
 
   play janela fundo fr it (desenha tiles) reageEventos reageTempo
   where
